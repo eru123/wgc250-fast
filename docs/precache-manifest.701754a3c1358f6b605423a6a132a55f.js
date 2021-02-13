99ffab52b39c7b83e651b83611aa34689968c0f7
Jericho Aquino <yeoligoakino@gmail.com>
2021-02-13 14:40:09 +0800

    Deploy

---
+++ docs/precache-manifest.701754a3c1358f6b605423a6a132a55f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03d585d8f648a729ce103eb92d8ca127",
+    "url": "/wgc250-fast/index.html"
+  },
+  {
+    "revision": "1d4ffb71ee8fab534d49",
+    "url": "/wgc250-fast/js/app.eace412d.js"
+  },
+  {
+    "revision": "74baf44e09ec31127d8be9d96e862ddc",
+    "url": "/wgc250-fast/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/wgc250-fast/robots.txt"
+  }
+]);
